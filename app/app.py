8a1f3071a8cb6006fa48a8845ab6240e61748a80
tanmayvakare <tanmayvakare@gmail.com>
2019-04-17 02:00:01 +0530

    Final version of website
    Completed all implementation and designs

--- app/app.py
+++ app/app.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request
 from SentimentAnalysis.PredictSentiment import predict_sentiment
 import requests as rq
+from json import dumps
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+
 	return render_template("index.html")
 
 @app.route("/predict")
 def predict():
+
 	prediction_pict = {0:"negative.png",1:"positive.png"}
 	
+	prediction_text = {0:"Your reponse seems negative.",1:"Your response seems positive."}
+
 	sentence = request.args.get("sentence")
 	prediction = predict_sentiment(sentence)
 
-	session["sentence"] = sentence
-	session["prediction"] = prediction
- 
-	return render_template("prediction.html",prediction_image = prediction_pict[prediction],prediction_text = sentence)
+	return render_template("prediction.html",prediction_image = prediction_pict[prediction],
+		prediction_text = prediction_text[prediction], prediction = str(prediction), sentence = sentence)
 
 @app.route("/review")
 def review():
 
-	data = { "Status" : request.args.get("check") == "1", "Prediction" : session["prediction"] == "1",
-	"Sentence" : session["sentence"], "Client" : requests.remote_addr }
+	data = { "Status" : request.args.get("check") == "1", "Prediction" : request.args.get("prediction") == "1",
+	"Sentence" : request.args.get("sentence"), "Client" : request.remote_addr, "TimeStamp" : str(datetime.now()) }
 
 	url = "https://sentibo-sentiment-analysis.firebaseio.com/reviews.json"
 
-	r = rq.post(url, data = data) 
+	res = rq.post(url, data = dumps(data)) 
+	
 
 	return render_template("thanks.html")
