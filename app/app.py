34098e9686741b5b62c9621f7663ae4df68f0107
tanmayvakare <tanmayvakare@gmail.com>
2019-04-16 16:40:33 +0530

    Updated UI and design
    Included review storing in firebase

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from SentimentAnalysis.PredictSentiment import predict_sentiment
+import requests as rq
 
 app = Flask(__name__)
 
@@ -17,8 +18,19 @@ def predict():
 	sentence = request.args.get("sentence")
 	prediction = predict_sentiment(sentence)
 
+	session["sentence"] = sentence
+	session["prediction"] = prediction
+ 
 	return render_template("prediction.html",prediction_image = prediction_pict[prediction],prediction_text = sentence)
 
 @app.route("/review")
 def review():
-	return
+
+	data = { "Status" : request.args.get("check") == "1", "Prediction" : session["prediction"] == "1",
+	"Sentence" : session["sentence"], "Client" : requests.remote_addr }
+
+	url = "https://sentibo-sentiment-analysis.firebaseio.com/reviews.json"
+
+	r = rq.post(url, data = data) 
+
+	return render_template("thanks.html")
